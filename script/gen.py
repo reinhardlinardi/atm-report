1ccdc29a1c62eae15463b369c24e050bcff40b4c
Reinhard Linardi <reinhardlinardi@gmail.com>
2025-03-06 22:38:19 +0700

    Gen script fix parse

--- script/gen.py
+++ script/gen.py
@@ -20,60 +20,53 @@ MAX_DAYS = 7
 MAX_ATM = 5
 MAX_TX = 10
 
-def valid_args():
-    if not len(sys.argv) == 4:
-        return False, 'Invalid number of cmd args'
-    
-    INVALID = 'Invalid args: '
-    
+class Args:
+    def __init__(self, argv):
+        self.days = int(argv[1])
+        self.atm = int(argv[2])
+        self.max_tx = int(argv[3])
+
+    def __str__(self):
+        return str(dict(days = self.days, atm = self.atm, max_tx = self.max_tx))
+
+def parse_args():
+    if not len(sys.argv) >= 4:
+        return None, err('invalid number of cmd args')
     if not sys.argv[1].isdigit():
-        return False, INVALID + 'number of days'
-    
+        return None, err('invalid number of days')
     if not sys.argv[2].isdigit():
-        return False, INVALID + 'number of ATM'
-
+        return None, err('invalid number of ATM')
     if not sys.argv[3].isdigit():
-        return False, INVALID + 'max number of transactions per ATM'
+        return None, err('invalid max number of transactions per ATM')
     
-    days = int(sys.argv[1])
-    atm = int(sys.argv[2])
-    max_tx = int(sys.argv[3])
+    args = Args(sys.argv)
 
-    if days > MAX_DAYS:
-        return False, INVALID + 'max ' + str(MAX_DAYS) + ' days'
-
-    if atm > MAX_ATM:
-        return False, INVALID + 'max ' + str(MAX_ATM) + ' ATM'
+    if args.days > MAX_DAYS:
+        return None, err('max number of days is {}'.format(MAX_DAYS))
+    if args.atm > MAX_ATM:
+        return None, err('max number of ATM is {}'.format(MAX_ATM))
+    if args.max_tx > MAX_TX:
+        return None, err('max number of transactions per ATM is {}'.format(MAX_TX))
     
-    if max_tx > MAX_TX:
-        return False, INVALID + 'max ' + str(MAX_TX) + ' transactions per ATM'
+    if args.days == 0:
+        args.days = random.randint(1, MAX_DAYS)
+    if args.atm == 0:
+        args.atm = random.randint(1, MAX_ATM)
+    if args.max_tx == 0:
+        args.max_tx = random.randint(1, MAX_TX)
 
-    return True, ''
+    return args, ''
 
-def get_args():
-    days = int(sys.argv[1])
-    atm = int(sys.argv[2])
-    max_tx = int(sys.argv[3])
-
-    if days == 0:
-        days = random.randint(1, MAX_DAYS)
-
-    if atm == 0:
-        atm = random.randint(1, MAX_ATM)
-
-    if max_tx == 0:
-        max_tx = random.randint(0, MAX_TX)
-
-    return {'days': days, 'atm': atm, 'max_tx': max_tx}
+def err(msg):
+    return 'err: {}'.format(msg)
 
 def main():
-    valid, msg = valid_args()
+    args, msg = parse_args()
 
-    if not valid:
-        print('FAIL:', msg)
+    if not args:
+        print(msg)
         return
     
-    print('OK')
-    print(get_args())
+    print(args)
 
 main()
