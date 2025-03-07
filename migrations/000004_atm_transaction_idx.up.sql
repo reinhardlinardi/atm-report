ca81ed15d46b45d14819d82094f5de5e2dd33837
Reinhard Linardi <reinhardlinardi@gmail.com>
2025-03-07 18:35:39 +0700

    Fix wrong index field

--- migrations/000004_atm_transaction_idx.up.sql
+++ migrations/000004_atm_transaction_idx.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `atm-report`.`atm_transaction` 
-ADD INDEX `transaction_date_type_idx` (`transaction_date` DESC, `transaction_time` ASC) VISIBLE;
+ADD INDEX `transaction_date_type_idx` (`transaction_date` DESC, `transaction_type` ASC) VISIBLE;
