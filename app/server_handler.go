233709fe456a5dfb380b0bcf4ac29a329f74c932
Reinhard Linardi <reinhardlinardi@gmail.com>
2025-03-10 09:37:08 +0700

    Tidy up API path

--- app/server_handler.go
+++ app/server_handler.go
@@ -15,23 +15,23 @@ import (
 func (srv *Server) RegisterHandlers() {
 	r := srv.router
 
+	r.Handle("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
+
 	url := fmt.Sprintf("http://localhost:%d/docs/swagger.json", srv.config.Port)
 	r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(url)))
 
-	r.Handle("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
-
-	r.Route("/api/v1/daily", func(r chi.Router) {
-		r.Get("/transaction-count", countTransactionHandler)
-		r.Get("/max-withdraw", maxWithdrawHandler)
+	r.Route("/api/v1/", func(r chi.Router) {
+		r.Get("/count", countTransaction)
+		r.Get("/max", maxWithdraw)
 	})
 }
 
-func countTransactionHandler(w http.ResponseWriter, r *http.Request) {
+func countTransaction(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("count transaction"))
 }
 
-func maxWithdrawHandler(w http.ResponseWriter, r *http.Request) {
+func maxWithdraw(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("max withdraw"))
 }
